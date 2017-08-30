0dbf6f2b5f025d5f4add32ace6ae45b85dcb8d17
DoaaAlsafadi <d.alsafadi.jo@gmail.com>
2017-08-30 09:33:06 +0300

    hash table

--- ds-hash-table/ds-hash-table.js
+++ ds-hash-table/ds-hash-table.js
@@ -1,34 +1,36 @@
-var makeHashTable = function() {
-  //Do not change the max!
-  var max = 4;
+		var makeHashTable = function() {
+		  //Do not change the max!
+		  var max = 4;
 
-    return {
-      _storage: [],
-      retrieve: function(key) {
-        return this._storage[hashFn(key, max)];
-      },
+		    return {
+		      _storage: [],
+		      retrieve: function(key) {
+		        return this._storage[hashFn(key, max)];
+		      },
 
-      insert: function(key, value) {
-        //your code is here
-        this._storage[hashFn(key, max)] = value;
+		      insert: function(key, value) {
+		        //your code is here
+		        //var hashIndex = hashFn(key, max);
 
-        for (var i = 0; i < this._storage.length; i++) {
-            if(this._storage.var === value){
-              this._storage[i-1] = key;
-            }
-        }
- 
-    }
-  }
-};
+		        if(this._storage[hashFn(key, max)] === undefined){
 
-// This is a "hashing function". You don't need to worry about it, just use it to turn any key into a pseudo-random key
-var hashFn = function(str, max) {
-  var hash = 0;
-  for (var i = 0; i < str.length; i++) {
-    var letter = str[i];
-    hash = (hash << 5) + letter.charCodeAt(0);
-    hash = (hash & hash) % max;
-  }
-  return hash;
-};
+		        	this._storage[hashFn(key, max)] = [];
+		        	this._storage[hashFn(key, max)].push([key, value]);
+		        }
+		        else{
+		        	this._storage[hashFn(key, max)].push([key, value]);
+		        }
+		 
+		}
+	}
+}
+		// This is a "hashing function". You don't need to worry about it, just use it to turn any key into a pseudo-random key
+		var hashFn = function(str, max) {
+		  var hash = 0;
+		  for (var i = 0; i < str.length; i++) {
+		    var letter = str[i];
+		    hash = (hash << 5) + letter.charCodeAt(0);
+		    hash = (hash & hash) % max;
+		  }
+		  return hash;
+		}
